4cf988c4056f9b24a46f17d632db9d73f530c97f
Boris <jelocnik.boris@gmail.com>
2018-01-12 15:15:39 +0100

    dodat podnaslov

--- kontakt-forma.php
+++ kontakt-forma.php
@@ -196,139 +196,74 @@ if (isset($_POST["submit"]) && $_POST["submit"] == "sendEmail") {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>Contact Form - Get in touch - feature 1</title>
+    <h3>Status</h3>
 
-        <!-- Bootstrap -->
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+ENW/yibaokMnme+vBLnHMphUYxHs34h9lpdbSLuAwGkOKFRl4C34WkjazBtb7eT" crossorigin="anonymous">
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+ENW/yibaokMnme+vBLnHMphUYxHs34h9lpdbSLuAwGkOKFRl4C34WkjazBtb7eT" crossorigin="anonymous">
 
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-        <script src='https://www.google.com/recaptcha/api.js'></script>
-    </head>
-    <body>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Cubes School</a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
-
-        <div class="container">
-            <div class="page-header">
-                <h1>Get in touch</h1>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Cubes School</a>
             </div>
-            <div class="row">
-                <div class="col-xs-12">
-                    <?php if (!empty($successMessage)) { ?>
-                        <div class="alert alert-successs alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <span> <?php echo htmlspecialchars($successMessage); ?></span>
-                        </div>
-                    <?php } ?>
-                    <?php if (!empty($errorMessage)) { ?>
-                        <div class="alert alert-danger alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <span><?php echo htmlspecialchars($errorMessage); ?></span>
-                        </div>
-                    <?php } ?>
-                </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="container">
+        <div class="page-header">
+            <h1>Get in touch</h1>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <?php if (!empty($successMessage)) { ?>
+                    <div class="alert alert-successs alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <span> <?php echo htmlspecialchars($successMessage); ?></span>
+                    </div>
+                <?php } ?>
+                <?php if (!empty($errorMessage)) { ?>
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <span><?php echo htmlspecialchars($errorMessage); ?></span>
+                    </div>
+                <?php } ?>
             </div>
-            <div class="row">
-                <form action="" method="post" role="form" class="form container-fluid">
-                    <fieldset class="row">
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <input class="form-control" name="name" id="name" value="<?php echo isset($formData["name"]) ? htmlspecialchars($formData["name"]) : ""; ?>" placeholder="Your Name" required="required" type="text">
-                                <div class="validation">
-                                    <?php
-                                    if (isset($formErrors["name"])) {
-                                        foreach ($formErrors["name"] as $errorMessage) {
-                                            ?>
-                                            <span class="text-danger"><?php echo $errorMessage; ?></span>
-                                            <?php
-                                        }
-                                    }
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <input class="form-control" name="email" id="email" value="<?php echo isset($formData["email"]) ? htmlspecialchars($formData["email"]) : ""; ?>" placeholder="Your Email" required="required" type="email">
-                                <div class="validation">
-                                    <?php
-                                    if (isset($formErrors["email"])) {
-                                        foreach ($formErrors["email"] as $errorMessage) {
-                                            ?>
-                                            <span class="text-danger"><?php echo $errorMessage; ?></span>
-                                            <?php
-                                        }
-                                    }
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="form-group">
-                                <input class="form-control" name="subject" id="subject" value="<?php echo isset($formData["subject"]) ? htmlspecialchars($formData["subject"]) : ""; ?>" placeholder="Subject" required="required" type="text">
-                                <div class="validation">
-                                    <?php
-                                    if (isset($formErrors["subject"])) {
-                                        foreach ($formErrors["subject"] as $errorMessage) {
-                                            ?>
-                                            <span class="text-danger"><?php echo $errorMessage; ?></span>
-                                            <?php
-                                        }
-                                    }
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <textarea class="form-control" name="message" rows="5" placeholder="Message" required="required"><?php echo isset($formData["message"]) ? htmlspecialchars($formData["message"]) : ""; ?></textarea>
-                                <div class="validation">
-                                    <?php
-                                    if (isset($formErrors["message"])) {
-                                        foreach ($formErrors["message"] as $errorMessage) {
-                                            ?>
-                                            <span class="text-danger"><?php echo $errorMessage; ?></span>
-                                            <?php
-                                        }
-                                    }
-                                    ?>
-                                </div>
-                            </div>
-                        </div>
-                    </fieldset>
-                    <fieldset class="row">
-                        <div class="col-md-6">
-                            <!-- OVDE IDE DIV ZA RECAPTCHA-u!!!-->
-                            <div class="g-recaptcha" data-sitekey="6LdNH0AUAAAAAMjny0TVfQJYkjGMEsO08rsmPhbQ"></div>
+        </div>
+        <div class="row">
+            <form action="" method="post" role="form" class="form container-fluid">
+                <fieldset class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input class="form-control" name="name" id="name" value="<?php echo isset($formData["name"]) ? htmlspecialchars($formData["name"]) : ""; ?>" placeholder="Your Name" required="required" type="text">
                             <div class="validation">
                                 <?php
-                                if (isset($formErrors["g-recaptcha-response"])) {
-                                    foreach ($formErrors["g-recaptcha-response"] as $errorMessage) {
+                                if (isset($formErrors["name"])) {
+                                    foreach ($formErrors["name"] as $errorMessage) {
                                         ?>
                                         <span class="text-danger"><?php echo $errorMessage; ?></span>
                                         <?php
@@ -337,19 +272,85 @@ if (isset($_POST["submit"]) && $_POST["submit"] == "sendEmail") {
                                 ?>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <button class="btn btn-default pull-right" type="submit" name="submit" value="sendEmail">Send Now</button>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input class="form-control" name="email" id="email" value="<?php echo isset($formData["email"]) ? htmlspecialchars($formData["email"]) : ""; ?>" placeholder="Your Email" required="required" type="email">
+                            <div class="validation">
+                                <?php
+                                if (isset($formErrors["email"])) {
+                                    foreach ($formErrors["email"] as $errorMessage) {
+                                        ?>
+                                        <span class="text-danger"><?php echo $errorMessage; ?></span>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                            </div>
                         </div>
-                    </fieldset>
-                </form>
-            </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input class="form-control" name="subject" id="subject" value="<?php echo isset($formData["subject"]) ? htmlspecialchars($formData["subject"]) : ""; ?>" placeholder="Subject" required="required" type="text">
+                            <div class="validation">
+                                <?php
+                                if (isset($formErrors["subject"])) {
+                                    foreach ($formErrors["subject"] as $errorMessage) {
+                                        ?>
+                                        <span class="text-danger"><?php echo $errorMessage; ?></span>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <textarea class="form-control" name="message" rows="5" placeholder="Message" required="required"><?php echo isset($formData["message"]) ? htmlspecialchars($formData["message"]) : ""; ?></textarea>
+                            <div class="validation">
+                                <?php
+                                if (isset($formErrors["message"])) {
+                                    foreach ($formErrors["message"] as $errorMessage) {
+                                        ?>
+                                        <span class="text-danger"><?php echo $errorMessage; ?></span>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
+                <fieldset class="row">
+                    <div class="col-md-6">
+                        <!-- OVDE IDE DIV ZA RECAPTCHA-u!!!-->
+                        <div class="g-recaptcha" data-sitekey="6LdNH0AUAAAAAMjny0TVfQJYkjGMEsO08rsmPhbQ"></div>
+                        <div class="validation">
+                            <?php
+                            if (isset($formErrors["g-recaptcha-response"])) {
+                                foreach ($formErrors["g-recaptcha-response"] as $errorMessage) {
+                                    ?>
+                                    <span class="text-danger"><?php echo $errorMessage; ?></span>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <button class="btn btn-default pull-right" type="submit" name="submit" value="sendEmail">Send Now</button>
+                    </div>
+                </fieldset>
+            </form>
         </div>
+    </div>
 
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
 </html>
 
