f5d40bbfb1da51d64555f945088ef237d62f6f10
Boris <jelocnik.boris@gmail.com>
2018-01-12 13:48:02 +0100

    dodat example fajl za config

---
+++ example-config.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'smtpHost' => '',
+    'smtpUsername' => '',
+    'smtpPassword' => '',
+    'recaptchaSecretKey' => '',
+//    '' => '',
+//    '' => '',
+//    '' => '',
+);
+
